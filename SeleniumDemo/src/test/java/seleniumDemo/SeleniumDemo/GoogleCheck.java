c6a0dc2560b73c3503bfd974bc30f1961ba96970
YUVRAJ <YUVRAJ@DESKTOP-GFMQBVM>
2022-07-23 15:53:48 +0530

    Added Wait Seconds

--- SeleniumDemo/src/test/java/seleniumDemo/SeleniumDemo/GoogleCheck.java
+++ SeleniumDemo/src/test/java/seleniumDemo/SeleniumDemo/GoogleCheck.java
@@ -1,5 +1,7 @@
 package seleniumDemo.SeleniumDemo;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -13,6 +15,7 @@ public class GoogleCheck {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.co.in");
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
 
 }
