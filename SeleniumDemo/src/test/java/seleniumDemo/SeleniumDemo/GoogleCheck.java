a8861848a5cee22de3b0f2710c17a8b1afa13fa6
YUVRAJ <YUVRAJ@DESKTOP-GFMQBVM>
2022-07-23 15:39:30 +0530

    1st Commit

---
+++ SeleniumDemo/src/test/java/seleniumDemo/SeleniumDemo/GoogleCheck.java
@@ -0,0 +1,17 @@
+package seleniumDemo.SeleniumDemo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleCheck {
+
+	@Test
+	public void googleTest() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\YUVRAJ\\git\\JenkinsGitDemo\\JenkinsGitDemo\\SeleniumDemo\\Drivers\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+	}
+
+}
