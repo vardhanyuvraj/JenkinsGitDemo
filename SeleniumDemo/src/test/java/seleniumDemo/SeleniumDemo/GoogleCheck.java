14865ded7d748512c08f8333e8f3b5a7b6fb8aa0
vardhanyuvraj <vardhan.yuvraj.singh@gmail.com>
2022-07-23 17:04:12 +0530

    checking cmd commit

--- SeleniumDemo/src/test/java/seleniumDemo/SeleniumDemo/GoogleCheck.java
+++ SeleniumDemo/src/test/java/seleniumDemo/SeleniumDemo/GoogleCheck.java
@@ -1,7 +1,5 @@
 package seleniumDemo.SeleniumDemo;
 
-import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
